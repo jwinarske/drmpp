f70e6df010a2545669989e6d71fffbf29fc92f8e
Joel Winarske <joel.winarske@gmail.com>
2024-11-25 18:26:57 -0800

    info update
    -fail over from fb2 to fb
    -add root object key as escaped path
    
    Signed-off-by: Joel Winarske <joel.winarske@gmail.com>

--- include/drmpp/shared_libs/libdrm.h
+++ include/drmpp/shared_libs/libdrm.h
@@ -17,173 +17,200 @@
 #ifndef INCLUDE_DRMPP_SHARED_LIBS_LIBDRM_H
 #define INCLUDE_DRMPP_SHARED_LIBS_LIBDRM_H
 
+#include <xf86drm.h>
 #include <xf86drmMode.h>
 
 struct LibDrmExports {
-    LibDrmExports() = default;
+  LibDrmExports() = default;
 
-    explicit LibDrmExports(void *lib);
+  explicit LibDrmExports(void* lib);
 
-    typedef int (*DrmIoctl)(int fd, unsigned long request, void *arg);
+  typedef int (*DrmIoctl)(int fd, unsigned long request, void* arg);
 
-    typedef drmModeConnectorPtr (*DrmModeGetConnector)(int fd,
-                                                       uint32_t connectorId);
+  typedef drmVersionPtr (*DrmGetVersion)(int fd);
 
-    typedef void (*DrmModeFreeConnector)(drmModeConnectorPtr ptr);
+  typedef void (*DrmFreeVersion)(drmVersionPtr);
 
-    typedef drmModeEncoderPtr (*DrmModeGetEncoder)(int fd, uint32_t encoder_id);
+  typedef drmModeConnectorPtr (*DrmModeGetConnector)(int fd,
+                                                     uint32_t connectorId);
 
-    typedef void (*DrmModeFreeEncoder)(drmModeEncoderPtr ptr);
+  typedef void (*DrmModeFreeConnector)(drmModeConnectorPtr ptr);
 
-    typedef drmModeCrtcPtr (*DrmModeGetCrtc)(int fd, uint32_t crtcId);
+  typedef drmModeEncoderPtr (*DrmModeGetEncoder)(int fd, uint32_t encoder_id);
 
-    typedef int (*DrmModeSetCrtc)(int fd,
-                                  uint32_t crtcId,
-                                  uint32_t bufferId,
-                                  uint32_t x,
-                                  uint32_t y,
-                                  uint32_t *connectors,
-                                  int count,
-                                  drmModeModeInfoPtr mode);
+  typedef void (*DrmModeFreeEncoder)(drmModeEncoderPtr ptr);
 
-    typedef void (*DrmModeFreeCrtc)(drmModeCrtcPtr ptr);
+  typedef drmModeCrtcPtr (*DrmModeGetCrtc)(int fd, uint32_t crtcId);
 
-    typedef drmModeFB2Ptr (*DrmModeGetFB2)(int fd, uint32_t bufferId);
+  typedef int (*DrmModeSetCrtc)(int fd,
+                                uint32_t crtcId,
+                                uint32_t bufferId,
+                                uint32_t x,
+                                uint32_t y,
+                                uint32_t* connectors,
+                                int count,
+                                drmModeModeInfoPtr mode);
 
-    typedef int (*DrmModeAddFB2)(int fd,
-                                 uint32_t width,
-                                 uint32_t height,
-                                 uint32_t pixel_format,
-                                 const uint32_t bo_handles[4],
-                                 const uint32_t pitches[4],
-                                 const uint32_t offsets[4],
-                                 uint32_t *buf_id,
-                                 uint32_t flags);
+  typedef void (*DrmModeFreeCrtc)(drmModeCrtcPtr ptr);
 
-    typedef int (*DrmModeAddFB2WithModifiers)(int fd,
-                                              uint32_t width,
-                                              uint32_t height,
-                                              uint32_t pixel_format,
-                                              const uint32_t bo_handles[4],
-                                              const uint32_t pitches[4],
-                                              const uint32_t offsets[4],
-                                              const uint64_t modifier[4],
-                                              uint32_t *buf_id,
-                                              uint32_t flags);
+  typedef drmModeFB2Ptr (*DrmModeGetFB2)(int fd, uint32_t bufferId);
 
-    typedef void (*DrmModeFreeFB2)(drmModeFB2Ptr ptr);
+  typedef int (*DrmModeAddFB2)(int fd,
+                               uint32_t width,
+                               uint32_t height,
+                               uint32_t pixel_format,
+                               const uint32_t bo_handles[4],
+                               const uint32_t pitches[4],
+                               const uint32_t offsets[4],
+                               uint32_t* buf_id,
+                               uint32_t flags);
 
-    typedef drmModeAtomicReqPtr (*DrmModeAtomicAlloc)();
+  typedef int (*DrmModeAddFB2WithModifiers)(int fd,
+                                            uint32_t width,
+                                            uint32_t height,
+                                            uint32_t pixel_format,
+                                            const uint32_t bo_handles[4],
+                                            const uint32_t pitches[4],
+                                            const uint32_t offsets[4],
+                                            const uint64_t modifier[4],
+                                            uint32_t* buf_id,
+                                            uint32_t flags);
 
-    typedef void (*DrmModeAtomicFree)(drmModeAtomicReqPtr req);
+  typedef void (*DrmModeFreeFB2)(drmModeFB2Ptr ptr);
 
-    typedef int (*DrmModeAtomicCommit)(int fd,
-                                       drmModeAtomicReqPtr req,
-                                       uint32_t flags,
-                                       void *user_data);
+  typedef drmModeAtomicReqPtr (*DrmModeAtomicAlloc)();
 
-    typedef int (*DrmModeAtomicAddProperty)(drmModeAtomicReqPtr req,
-                                            uint32_t object_id,
-                                            uint32_t property_id,
-                                            uint64_t value);
+  typedef void (*DrmModeAtomicFree)(drmModeAtomicReqPtr req);
 
-    typedef int (*DrmModeAtomicGetCursor)(drmModeAtomicReqPtr req);
+  typedef int (*DrmModeAtomicCommit)(int fd,
+                                     drmModeAtomicReqPtr req,
+                                     uint32_t flags,
+                                     void* user_data);
 
-    typedef void (*DrmModeAtomicSetCursor)(drmModeAtomicReqPtr req, int cursor);
+  typedef int (*DrmModeAtomicAddProperty)(drmModeAtomicReqPtr req,
+                                          uint32_t object_id,
+                                          uint32_t property_id,
+                                          uint64_t value);
 
-    typedef drmModePropertyPtr (*DrmModeGetProperty)(int fd, uint32_t propertyId);
+  typedef int (*DrmModeAtomicGetCursor)(drmModeAtomicReqPtr req);
 
-    typedef void (*DrmModeFreeProperty)(drmModePropertyPtr ptr);
+  typedef void (*DrmModeAtomicSetCursor)(drmModeAtomicReqPtr req, int cursor);
 
-    typedef drmModePropertyBlobPtr (*DrmModeGetPropertyBlob)(int fd,
-                                                             uint32_t blob_id);
+  typedef drmModePropertyPtr (*DrmModeGetProperty)(int fd, uint32_t propertyId);
 
-    typedef void (*DrmModeFreePropertyBlob)(drmModePropertyBlobPtr ptr);
+  typedef void (*DrmModeFreeProperty)(drmModePropertyPtr ptr);
 
-    typedef void (*DrmModeFreeObjectProperties)(drmModeObjectPropertiesPtr ptr);
+  typedef drmModePropertyBlobPtr (*DrmModeGetPropertyBlob)(int fd,
+                                                           uint32_t blob_id);
 
-    typedef int (*DrmModeCreatePropertyBlob)(int fd,
-                                             const void *data,
-                                             size_t size,
-                                             uint32_t *id);
+  typedef void (*DrmModeFreePropertyBlob)(drmModePropertyBlobPtr ptr);
 
-    typedef drmModeObjectPropertiesPtr (*DrmModeObjectGetProperties)(
-        int fd,
-        uint32_t object_id,
-        uint32_t object_type);
+  typedef void (*DrmModeFreeObjectProperties)(drmModeObjectPropertiesPtr ptr);
 
-    typedef drmModeResPtr (*DrmModeGetResources)(int fd);
+  typedef int (*DrmModeCreatePropertyBlob)(int fd,
+                                           const void* data,
+                                           size_t size,
+                                           uint32_t* id);
 
-    typedef void (*DrmModeFreeResources)(drmModeResPtr ptr);
+  typedef drmModeObjectPropertiesPtr (*DrmModeObjectGetProperties)(
+      int fd,
+      uint32_t object_id,
+      uint32_t object_type);
 
-    typedef drmModePlaneResPtr (*DrmModeGetPlaneResources)(int fd);
+  typedef drmModeResPtr (*DrmModeGetResources)(int fd);
 
-    typedef void (*DrmModeFreePlaneResources)(drmModePlaneResPtr ptr);
+  typedef void (*DrmModeFreeResources)(drmModeResPtr ptr);
 
-    typedef drmModePlanePtr (*DrmModeGetPlane)(int fd, uint32_t plane_id);
+  typedef drmModePlaneResPtr (*DrmModeGetPlaneResources)(int fd);
 
-    typedef void (*DrmModeFreePlane)(drmModePlanePtr ptr);
+  typedef void (*DrmModeFreePlaneResources)(drmModePlaneResPtr ptr);
 
-    typedef int (*DrmModeRmFB)(int fd, uint32_t bufferId);
+  typedef drmModePlanePtr (*DrmModeGetPlane)(int fd, uint32_t plane_id);
 
-    typedef int (*DrmModeAddFB)(int fd,
-                                uint32_t width,
-                                uint32_t height,
-                                uint8_t depth,
-                                uint8_t bpp,
-                                uint32_t pitch,
-                                uint32_t bo_handle,
-                                uint32_t *buf_id);
+  typedef void (*DrmModeFreePlane)(drmModePlanePtr ptr);
 
-    typedef int (*DrmSetClientCap)(int fd, uint64_t capability, uint64_t value);
+  typedef int (*DrmModeRmFB)(int fd, uint32_t bufferId);
 
-    DrmIoctl Ioctl = nullptr;
-    DrmSetClientCap SetClientCap = nullptr;
+  typedef int (*DrmModeAddFB)(int fd,
+                              uint32_t width,
+                              uint32_t height,
+                              uint8_t depth,
+                              uint8_t bpp,
+                              uint32_t pitch,
+                              uint32_t bo_handle,
+                              uint32_t* buf_id);
 
-    DrmModeGetConnector ModeGetConnector = nullptr;
-    DrmModeFreeConnector ModeFreeConnector = nullptr;
-    DrmModeGetEncoder ModeGetEncoder = nullptr;
-    DrmModeFreeEncoder ModeFreeEncoder = nullptr;
-    DrmModeGetCrtc ModeGetCrtc = nullptr;
-    DrmModeSetCrtc ModeSetCrtc = nullptr;
-    DrmModeFreeCrtc ModeFreeCrtc = nullptr;
-    DrmModeGetFB2 ModeGetFB2 = nullptr;
-    DrmModeAddFB ModeAddFB = nullptr;
-    DrmModeAddFB2 ModeAddFB2 = nullptr;
-    DrmModeAddFB2WithModifiers ModeAddFB2WithModifiers = nullptr;
-    DrmModeFreeFB2 ModeFreeFB2 = nullptr;
-    DrmModeAtomicAlloc ModeAtomicAlloc = nullptr;
-    DrmModeAtomicFree ModeAtomicFree = nullptr;
-    DrmModeAtomicCommit ModeAtomicCommit = nullptr;
-    DrmModeAtomicAddProperty ModeAtomicAddProperty = nullptr;
-    DrmModeAtomicGetCursor ModeAtomicGetCursor = nullptr;
-    DrmModeAtomicSetCursor ModeAtomicSetCursor = nullptr;
-    DrmModeGetProperty ModeGetProperty = nullptr;
-    DrmModeFreeProperty ModeFreeProperty = nullptr;
-    DrmModeGetPropertyBlob ModeGetPropertyBlob = nullptr;
-    DrmModeFreePropertyBlob ModeFreePropertyBlob = nullptr;
-    DrmModeFreeObjectProperties ModeFreeObjectProperties = nullptr;
-    DrmModeCreatePropertyBlob ModeCreatePropertyBlob = nullptr;
-    DrmModeObjectGetProperties ModeObjectGetProperties = nullptr;
-    DrmModeGetResources ModeGetResources = nullptr;
-    DrmModeFreeResources ModeFreeResources = nullptr;
-    DrmModeGetPlaneResources ModeGetPlaneResources = nullptr;
-    DrmModeFreePlaneResources ModeFreePlaneResources = nullptr;
-    DrmModeGetPlane ModeGetPlane = nullptr;
-    DrmModeFreePlane ModeFreePlane = nullptr;
-    DrmModeRmFB ModeRmFB = nullptr;
+  typedef int (*DrmSetClientCap)(int fd, uint64_t capability, uint64_t value);
+
+  typedef int (*DrmGetCap)(int fd, uint64_t capability, uint64_t* value);
+
+  typedef int (*DrmGetDevice)(int fd, drmDevicePtr* device);
+
+  typedef void (*DrmFreeDevice)(drmDevicePtr* device);
+
+  typedef drmModeFBPtr (*DrmModeGetFB)(int fd, uint32_t bufferId);
+
+  typedef void (*DrmModeFreeFB)(drmModeFBPtr ptr);
+
+  typedef drmModeConnectorPtr (
+      *DrmModeGetConnectorCurrent)(int fd, uint32_t connector_id);
+
+  DrmIoctl Ioctl = nullptr;
+  DrmSetClientCap SetClientCap = nullptr;
+
+  DrmModeGetConnectorCurrent ModeGetConnectorCurrent = nullptr;
+  DrmModeGetConnector ModeGetConnector = nullptr;
+  DrmModeFreeConnector ModeFreeConnector = nullptr;
+  DrmModeGetEncoder ModeGetEncoder = nullptr;
+  DrmModeFreeEncoder ModeFreeEncoder = nullptr;
+  DrmModeGetCrtc ModeGetCrtc = nullptr;
+  DrmModeSetCrtc ModeSetCrtc = nullptr;
+  DrmModeFreeCrtc ModeFreeCrtc = nullptr;
+  DrmModeGetFB ModeGetFB = nullptr;
+  DrmModeGetFB2 ModeGetFB2 = nullptr;
+  DrmModeAddFB ModeAddFB = nullptr;
+  DrmModeAddFB2 ModeAddFB2 = nullptr;
+  DrmModeAddFB2WithModifiers ModeAddFB2WithModifiers = nullptr;
+  DrmModeFreeFB ModeFreeFB = nullptr;
+  DrmModeFreeFB2 ModeFreeFB2 = nullptr;
+  DrmModeAtomicAlloc ModeAtomicAlloc = nullptr;
+  DrmModeAtomicFree ModeAtomicFree = nullptr;
+  DrmModeAtomicCommit ModeAtomicCommit = nullptr;
+  DrmModeAtomicAddProperty ModeAtomicAddProperty = nullptr;
+  DrmModeAtomicGetCursor ModeAtomicGetCursor = nullptr;
+  DrmModeAtomicSetCursor ModeAtomicSetCursor = nullptr;
+  DrmModeGetProperty ModeGetProperty = nullptr;
+  DrmModeFreeProperty ModeFreeProperty = nullptr;
+  DrmModeGetPropertyBlob ModeGetPropertyBlob = nullptr;
+  DrmModeFreePropertyBlob ModeFreePropertyBlob = nullptr;
+  DrmModeFreeObjectProperties ModeFreeObjectProperties = nullptr;
+  DrmModeCreatePropertyBlob ModeCreatePropertyBlob = nullptr;
+  DrmModeObjectGetProperties ModeObjectGetProperties = nullptr;
+  DrmModeGetResources ModeGetResources = nullptr;
+  DrmModeFreeResources ModeFreeResources = nullptr;
+  DrmModeGetPlaneResources ModeGetPlaneResources = nullptr;
+  DrmModeFreePlaneResources ModeFreePlaneResources = nullptr;
+  DrmModeGetPlane ModeGetPlane = nullptr;
+  DrmModeFreePlane ModeFreePlane = nullptr;
+  DrmModeRmFB ModeRmFB = nullptr;
+
+  DrmGetVersion GetVersion = nullptr;
+  DrmFreeVersion FreeVersion = nullptr;
+  DrmGetCap GetCap = nullptr;
+  DrmGetDevice GetDevice = nullptr;
+  DrmFreeDevice FreeDevice = nullptr;
 };
 
 class drm {
-public:
-    static bool IsPresent(const char *library_path = nullptr) {
-        return loadExports(library_path) != nullptr;
-    }
+ public:
+  static bool IsPresent(const char* library_path = nullptr) {
+    return loadExports(library_path) != nullptr;
+  }
 
-    LibDrmExports *operator->() const;
+  LibDrmExports* operator->() const;
 
-private:
-    static LibDrmExports *loadExports(const char *library_path);
+ private:
+  static LibDrmExports* loadExports(const char* library_path);
 };
 
 extern drm drm;
